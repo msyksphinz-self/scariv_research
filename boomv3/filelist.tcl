ecaff0149923cbba90f2de5b885bf421e364d01e
msyksphinz <msyksphinz.dev@gmail.com>
2024-01-28 15:11:23 +0900

    Update MediumBoomConfig

---
+++ boomv3/filelist.tcl
@@ -0,0 +1,8 @@
+add_files -norecurse chipyard.TestHarness.MediumBoomConfig.harness.mems.v
+add_files -norecurse chipyard.TestHarness.MediumBoomConfig.harness.v
+add_files -norecurse chipyard.TestHarness.MediumBoomConfig.top.mems.v
+add_files -norecurse chipyard.TestHarness.MediumBoomConfig.top.v
+add_files -norecurse ClockDividerN.sv
+add_files -norecurse EICG_wrapper.v
+add_files -norecurse IOCell.v
+add_files -norecurse plusarg_reader.v
